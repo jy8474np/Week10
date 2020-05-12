ece44311625f7982aee45b982e838a81243884ea
“jy8474np” <jy8474np@go.minnstate.edu>
2020-05-12 18:11:21 -0500

    Updated API message

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ let express = require('express')
 let router = express.Router()
 
 router.get('/', function (req, res, next) {
-    res.json( { 'message': 'Hello ITEC 2560 web programmers!' } )
+    res.json( { 'message': 'Hello AWESOME ITEC 2560 web programmers!' } )
 })
 
 module.exports = router
